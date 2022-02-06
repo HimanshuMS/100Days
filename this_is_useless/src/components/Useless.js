54c117f1f9dba7c61965658b2ec79d98895125d9
HimanshuMS <himanshushah1215@gmail.com>
2022-02-06 23:55:37 +0530

    improvements

--- this_is_useless/src/components/Useless.js
+++ this_is_useless/src/components/Useless.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import '../index.css';
 
 function Useless() {
   return (
     <>
       <h1>Useless</h1>
-      <div>
-        <input type="text" id='useless' placeholder='type here' onChange={(e) => {
-          var i = e.target.value.length
-          while(i > 0) {
-            setTimeout(() => {e.target.value = e.target.value.slice(0, -1)}, 1000)
-            i--
-          }
-        } } />
+      <div className="useless">
+        <table>
+          <thead>
+            <tr>
+              <td>...</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+              <input type="text" placeholder='type here' onChange={(e) => {
+                  var i = e.target.value.length
+                  while(i > 0) {
+                    setTimeout(() => {e.target.value = e.target.value.slice(0, -1)}, 500)
+                    i--
+                  }
+                }} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
